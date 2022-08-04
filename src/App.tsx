c9eb37de41a0e17c835d15c9b2fbc5d42e1750f3
Lhester Monroyo <lhestermoon3011@gmail.com>
2022-08-04 08:22:55 +0800

    router to browserrouter

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,12 @@ import 'antd/dist/antd.css';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import Header from './components/Header';
-import RegionList from './components/RegionList';
 import AppRoutes from './routes';
 
 const App = () => {
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Header />
         <AppRoutes />
       </BrowserRouter>
