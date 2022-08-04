31893a42483a8c1c28493986c9043ab7a44bb1ff
Lhester Monroyo <lhestermoon3011@gmail.com>
2022-08-04 08:26:22 +0800

    revert router

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import 'antd/dist/antd.css';
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import Header from './components/Header';
 import AppRoutes from './routes';
 
 const App = () => {
   return (
     <div className="App">
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <HashRouter>
         <Header />
         <AppRoutes />
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 };
